b3b1bdffbb0f85e92a4c32be9e29112afe8cb4bf
Prateek Sachdev <prateek.sachdev92@gmail.com>
2013-05-24 04:39:04 +0530

    spoj : CHAIR  choose k out of n chairs ..such that no two adjacent

---
+++ circular_selection.cpp
@@ -0,0 +1,101 @@
+//
+//  main.cpp
+//
+//  Created by Prateek Sachdev 
+//  Copyright (c) 2013 Prateek Sachdev. All rights reserved.
+//
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+
+using namespace std;
+
+typedef vector<int > vd;
+
+#define sd(n) scanf("%d",&n)
+#define sc(n) scanf("%c",&n)
+#define sf(n) scanf("%f",&n)
+#define ss(n) scanf("%s",n)
+#define pd(n) printf("%d\n",n)
+#define pb push_back
+
+#define ALL(c) c.begin(), c.end()
+#define UB(s, e, x) upper_bound(s, e, x)
+#define LB(s, e, x) lower_bound(s, e, x)
+#define REV(s, e) reverse(s, e);
+#define SZ(c) c.size()
+#define SET(p) memset(p, -1, sizeof(p))
+#define CLR(p) memset(p, 0, sizeof(p))
+#define MEM(p, v) memset(p, v, sizeof(p))
+
+#define SIEVE(a) ({int b=ceil(sqrt(a));vd d(a,0);vd e;int f=2;e.pb(2);e.pb(3);for(int x=1;x<b+1;x++){for(int y=1;y<b+1;y++){int n=(4*x*x)+(y*y);if(n<=a&&(n%12==1||n%12==5)){d[n]^=1;}n=(3*x*x)+(y*y);if(n<=a&&n%12==7){d[n]^=1;}n=(3*x*x)-(y*y);if(x>y&&n<=a&&n%12==11){d[n]^=1;}}}for(int r=5;r<b+1;r++){if(d[r]){for(int i=r*r;i<a;i+=(r*r)){d[i]=0;}}}for(int c=5;c<a;c++){if(d[c]){e.pb(c);}}e;})
+template<class T> inline vector<pair<T,int> > FACTORISE(T n){vector<pair<T,int> >R;for (T i=2;n>1;){if (n%i==0){int C=0;for (;n%i==0;C++,n/=i);R.push_back(make_pair(i,C));}i++;if (i>n/i) i=n;}if (n>1) R.push_back(make_pair(n,1));return R;}
+template<class T> inline T TOTIENT(T n) {vector<pair<T,int> > R=FACTORISE(n);T r=n;for (int i=0;i<R.size();i++)r=r/R[i].first*(R[i].first-1);return r;}
+
+// vd v=SIEVE(2100);
+// int n=TOTIENT(num);
+#define MOD 1000000003
+int res;
+int place[2][1002][502];
+int N,K;
+int place_in_circle(int start,int pos,int kth)
+{
+	if(kth==K)
+		return 1;
+	if(pos==N && start==1)
+		return 0;
+	if(pos>N)
+		return 0;
+	if(place[start][pos][kth]!=-1)
+		return place[start][pos][kth];
+	if(pos==1)
+	{
+		place[start][pos][kth]= place_in_circle(1,pos+2,kth+1);
+		
+	}
+	else
+	{
+		place[start][pos][kth]= place_in_circle(start,pos+2,kth+1);
+		
+	}
+	place[start][pos][kth]=(place[start][pos][kth]+place_in_circle(start,pos+1,kth))%MOD;
+	return place[start][pos][kth];
+}
+int main()
+{
+	
+	sd(N);
+	sd(K);
+	if(K>N/2)
+		printf("0\n");
+	else
+	{
+		SET(place);
+		res=place_in_circle(0,1,0);
+		printf("%d\n",res );
+	}
+
+	
+
+	return 0;
+}
+
+
+
